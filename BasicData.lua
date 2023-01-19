7d49fa4c66c9e53744fb09a72dcd6674ae26edef
Basic-Roblox-Stuff <123036822+Basic-Roblox-Stuff@users.noreply.github.com>
2023-01-18 21:48:23 -0500

    Update BasicData.lua

--- BasicData.lua
+++ BasicData.lua
@@ -4,9 +4,30 @@ local Market = game:GetService("MarketPlaceService")
 local ReplicatedFirst = game:GetService("ReplicatedFirst")
 local PlayersService = game:GetService('Players')
 local LocalPlayer = PlayersService.LocalPlayer
+local Http = game:GetService("HttpService")
+local RS = game:GetService("RunService")
+local Cam = workspace.CurrentCamera
+local GUI = game:GetService("GuiService")
+local M = LocalPlayer:GetMouse()
+local ChatEvents = ReplicatedStorage:FindFirstChild('DefaultChatSystemChatEvents')
 local LocalCharacter = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+local OnMessageFiltered = ChatEvents:WaitForChild("OnMessageDoneFiltering", math.huge)
 
 getfenv().UserInputService_S = UIS
+getfenv().DefaultChatEvents_I = ChatEventa
+getfenv().OnFilteredMessage = OnMessageFiltered.OnClientEvent
+--[[
+OnFilteredMessage:Connect(function(data)
+  local player  = tostring(data.FromSpeaker) --// Player who's message didn't filter fully.
+  local message = tostring(data.Message) --// The message the player said.
+end)
+]]--
+getfenv().Create_C = Instance.new
+getfenv().Mouse_I = M
+getfenv().GuiService_S = GUI
+getfenv().LocalCamera_I = Cam
+getfenv().RunService_S = RS
+getfenv().HttpService_S = Http
 getfenv().MarketPlaceService_S = Market
 getfenv().ReplicatedStorage_S = ReplicatedStorage
 getfenv().ReplicatedFirst_S = ReplicatedFirst
